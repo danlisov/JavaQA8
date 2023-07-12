9bbf7cbbeebc777b7b3873a64c9fbadb52751f94
Denis Lysov <dan.lysov2015@yandex.ru>
2023-07-12 19:35:44 +0300

    Initial commit

---
+++ src/main/java/ru/netology/javaqa/javaqa8/service/FormDate.java
@@ -0,0 +1,7 @@
+package ru.netology.javaqa.javaqa8.service;
+
+public class FormDate {
+    public int day;
+    public int month;
+    public int year;
+}
---
+++ src/main/java/ru/netology/javaqa/javaqa8/service/Post.java
@@ -0,0 +1,10 @@
+package ru.netology.javaqa.javaqa8.service;
+public class Post {
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public boolean subscription;
+    public FormDate birthday;
+}
